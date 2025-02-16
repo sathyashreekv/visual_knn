a5268d33db16c948acde61c6b8a1ab2c4a9a269c
sathyashreekv <155656440+sathyashreekv@users.noreply.github.com>
2025-02-16 10:07:36 +0530

    Update knn.py

--- knn.py
+++ knn.py
@@ -1,8 +1,7 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
+
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.neighbors import KNeighborsClassifier
